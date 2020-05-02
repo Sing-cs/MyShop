ad7baf4675d0fc44c9d6a5b3231a51b4e854bb7e
Sing-cs <38367313+Sing-cs@users.noreply.github.com>
2020-05-03 00:00:19 +0800

    Added Order Management

---
+++ MyShop/MyShop.WebUI/Controllers/OrderManagerController.cs
@@ -0,0 +1,58 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+
+        public OrderManagerController(IOrderService OrderService)
+        {
+            this.orderService = OrderService;
+        }
+        // GET: OrderManager
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList();
+            return View(orders);
+        }
+
+        public ActionResult UpdateOrder(string Id)
+        {
+            ViewBag.StateList = new List<string>()
+            {
+                "Order Created",
+                "Payment Processed",
+                "OrderShipped",
+                "Order Completed"
+            };
+
+            Order order = orderService.GetOrder(Id);
+
+            if (order != null)
+            {
+                return View(order);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult UpdateOrder(Order updatedOrder, string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+            order.OrderState = updatedOrder.OrderState;
+            orderService.UpdateOrder(order);
+            return RedirectToAction("Index");
+        }
+    }
+}
